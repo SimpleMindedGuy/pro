e664a6b2e23fa3c12867e2bc771a4cd1aaadbe7e
Sufian <simpleminded@tutamail.com>
2023-08-28 21:59:36 +0300

    adding previous records

--- src/Pages/DashBoard.jsx
+++ src/Pages/DashBoard.jsx
@@ -7,6 +7,7 @@ import {firebase,DataBase} from '../utils/FirebaseInit'
 import { onValue, ref } from 'firebase/database';
 
 import BG from "../assets/Image/BG.jpg";
+import WeatherCard from '../components/WeatherCard';
 
 const DB = getFirestore(firebase)
 
@@ -60,44 +61,53 @@ function DashBoard() {
 
     
     
-    
     return () => {
 
     }
   }, [Weather])
   
+  let date = new Date();
   
   return (
     
     <>
         <h1>Smart Weather System</h1>
         { Weather ? 
-          <section className='Today' style={{backgroundImage: `url('${BG}')` }}>
-            <div className='Weather-Info'>
-            <img src='https://www.svgrepo.com/show/407540/sun.svg'/>
+        <>
+            <section className='Today' style={{backgroundImage: `url('${BG}')` }}>
+              <div className='Weather-Info'>
+              <h4>{date.toLocaleDateString("en-UK",{year: "numeric",  month : "2-digit", day:"2-digit"})}</h4>
+              <img src='https://www.svgrepo.com/show/407540/sun.svg'/>
 
-            
-            <div> 
-              { Weather.Temperature ? <h4>{Weather.Temperature}C</h4> : <p>"nope"</p>}
-              <h3>الحرارة</h3>
-            </div>
+              
+              <div> 
+                { Weather.Temperature ? <h4>{Weather.Temperature}C</h4> : <p>"nope"</p>}
+                <h3>الحرارة</h3>
+              </div>
 
-            <div>
-              { Weather.Pressure ? <h4>{Weather.Pressure} pa</h4> : <p>"nope"</p>} 
-              <h3>الضغط الجوي</h3>
+              <div>
+                { Weather.Pressure ? <h4>{Weather.Pressure} pa</h4> : <p>"nope"</p>} 
+                <h3>الضغط الجوي</h3>
+              </div>
+    
+              <h3 className='FK'> و تكون الحالة الجوية 
+                {
+                  Weather.Temperature >= 40 ? <> حار جدا </> 
+                  : Weather.Temperature >= 35 ? <> حار </> 
+                  : Weather.Temperature >= 30 ? <> معتدل صيفي </> 
+                  : <> معتدل ربيعي </>
+                } 
+                في جميع مناطق المملكة </h3>
             </div>
-  
-            <h3 className='FK'> و تكون الحالة الجوية 
-              {
-                Weather.Temperature >= 40 ? <> حار جدا </> 
-                : Weather.Temperature >= 35 ? <> حار </> 
-                : Weather.Temperature >= 30 ? <> معتدل صيفي </> 
-                : <> معتدل ربيعي </>
-              } 
-              في جميع مناطق المملكة </h3>
-          </div>
+            </section>
+
+          <h2>Previous Records </h2>
+          <section className='Records'>
+            <WeatherCard max={35} min={24} date={new Date((date.getDate()  - 1 ))} ></WeatherCard>
+            <WeatherCard max={32} min={21} date={new Date((date.getDate()  - 2 ))} ></WeatherCard>
+            <WeatherCard max={29} min={17} date={new Date((date.getDate()  - 3 ))}></WeatherCard>
           </section>
-
+        </>
           : 
           <h1> No weather information</h1>
         }
--- src/components/WeatherCard.jsx
+++ src/components/WeatherCard.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
 
-export default function WeatherCard(props) {
+
+export default function WeatherCard({max,min,date},props) {
+
+
   return (
     <>
         <div className='Card'>
+            <h6>{date.toLocaleDateString('en-UK',{weekday : "long"})}</h6>
             <img src='https://www.svgrepo.com/show/407540/sun.svg'/>
-            <p>Number</p>
+            <p>{max ? max : null}</p>
+            <p>{min ? min : null}</p>
         </div>
     </>
   )
